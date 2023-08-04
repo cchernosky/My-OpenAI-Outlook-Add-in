b8e4514763fed4550ff2390e1f89153b2a41a02f
cchernosky <49447501+cchernosky@users.noreply.github.com>
2023-08-04 15:59:55 -0600

    bvadb

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -8,19 +8,28 @@
 import { runCompletion } from "../../index.js";
 var body = "";
 
+// Called when the task bar button is pressed
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
-    //run();
+
+    toggleJumpingDots("hide");
+
+    // Pre-hydrate data structures
+    Office.context.mailbox.item.body.getAsync(Office.CoercionType.Text, (asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        setBody(asyncResult.value);
+      }
+    });
   }
 });
 
 export async function run() {
   const item = Office.context.mailbox.item;
 
-  document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+  toggleJumpingDots("show");
 
   await item.body.getAsync(Office.CoercionType.Text, (asyncResult) => {
     if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
@@ -30,12 +39,35 @@ export async function run() {
     }
   });
 
-  //document.getElementById("item-body0").innerHTML = "<b>Body:</b> <br/>" + body.length;
+  if (body != "") {
+    document.getElementById("item-subject").innerHTML = "<h2>Subject</h2>" + item.subject;
+    document.getElementById("item-body").innerHTML = (
+      await runCompletion(
+        "Please provide key points and actions as separate bulleted HTML sections with H2 headings: " + body
+      )
+    ).text;
+  } else {
+    document.getElementById("item-body").innerHTML =
+      // eslint-disable-next-line prettier/prettier
+      "<h2>Key Points & Actions:</h2> <br/>" + "Body empty";
+  }
 
-  document.getElementById("item-body").innerHTML =
-    "<b>Result:</b> <br/>" + (await runCompletion("please list key points and actions: " + body)).text;
+  toggleJumpingDots("hide");
 }
 
 function setBody(asyncResultParam) {
   body = asyncResultParam;
+}
+
+function toggleJumpingDots(task) {
+  var jumpingdots = document.getElementById("jumpingdots");
+  var spacer = document.getElementById("spacer");
+
+  if (task === "hide") {
+    jumpingdots.style.display = "none";
+    spacer.style.display = "block";
+  } else {
+    jumpingdots.style.display = "block";
+    spacer.style.display = "none";
+  }
 }
